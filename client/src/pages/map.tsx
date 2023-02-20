1baac464f8da9d9357474c434fe11bd3ae9f7d12
tangoyankee <miller.tim108@gmail.com>
2023-02-20 17:01:14 -0500

    load map to algeria

--- client/src/pages/map.tsx
+++ client/src/pages/map.tsx
@@ -1,11 +1,21 @@
-import { Box, Text } from "@chakra-ui/react";
-import ReactMapGL, {Source, Layer } from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import { Box } from "@chakra-ui/react";
+import ReactMapGL, { Source, Layer } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const algeriaBorderLayerStyle = {
+  id: "algeria-border",
+  source: "algeria-border",
+  type: "fill" as const,
+  paint: {
+    "fill-color": "#0080ff",
+    "fill-opacity": 0.5,
+  },
+};
 
 const MapPage = () => (
   <Box height="100%" flex="1">
     <ReactMapGL
-      initialViewState ={{
+      initialViewState={{
         longitude: 2.632,
         latitude: 28.163,
         zoom: 4,
@@ -13,7 +23,13 @@ const MapPage = () => (
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
     >
-      <Source id="algera-border" type="geojson" data={"http://localhost:8000/world/border/2?format=json"}></Source>
+      <Source
+        id="algeria-border"
+        type="geojson"
+        data={"http://localhost:8000/world/border/2/?format=json"}
+      >
+        <Layer {...algeriaBorderLayerStyle} />
+      </Source>
     </ReactMapGL>
   </Box>
 );
