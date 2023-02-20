d18a8a8c0f13db0b97807d834337e75d63008e5d
tangoyankee <miller.tim108@gmail.com>
2023-02-20 14:49:23 -0500

    render react map

--- client/src/pages/map.tsx
+++ client/src/pages/map.tsx
@@ -1,8 +1,18 @@
 import { Box, Text } from "@chakra-ui/react";
+import ReactMapGL from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MapPage = () => (
   <Box height="100%" flex="1">
-    <Text color="text" height="100%">Map page</Text>
+    <ReactMapGL
+      initialViewState ={{
+        longitude: 2.632,
+        latitude: 28.163,
+        zoom: 4,
+      }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+    />
   </Box>
 );
 
