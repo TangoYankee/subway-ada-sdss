cf6f445a69b247ac648c9b3a27de0de27b11c5de
tangoyankee <miller.tim108@gmail.com>
2023-02-21 19:59:20 -0500

    make json the default return style

--- api/api/settings.py
+++ api/api/settings.py
@@ -63,6 +63,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'api.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
