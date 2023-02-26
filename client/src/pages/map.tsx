5c1d48b1048c5664ab0444758c0771cf788446bd
Tim Miller <miller.tim108@gmail.com>
2023-02-25 23:21:50 -0500

    map express bus routes

--- client/src/pages/map.tsx
+++ client/src/pages/map.tsx
@@ -3,12 +3,12 @@ import ReactMapGL, { Source, Layer } from "react-map-gl";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 
-const DOMAIN = "http://localhost:8000"
+const DOMAIN = "http://localhost:8000";
 
 const busRouteLayerStyle = {
   id: "bus-routes",
   source: "bus-routes",
-  type: "line" as "line",
+  type: "line" as const,
   paint: {
     "line-color": ["get", "color"] as unknown,
     "line-opacity": 0.5,
@@ -18,20 +18,30 @@ const busRouteLayerStyle = {
 const busStopLayerStyle = {
   id: "bus-stops",
   source: "bus-stops",
-  type: "circle" as "circle",
+  type: "circle" as const,
   paint: {
     "circle-color": "#222823",
     "circle-radius": 1.5,
-    "circle-opacity": 0.5
-  }
-}
+    "circle-opacity": 0.5,
+  },
+};
+
+const busRouteExpressLayerStyle = {
+  id: "bus-routes-express",
+  source: "bus-routes-express",
+  type: "line" as const,
+  paint: {
+    "line-color": ["get", "color"] as unknown,
+    "line-opacity": 0.5,
+  },
+};
 
 const MapPage = () => (
   <Box height="100%" flex="1">
     <ReactMapGL
       mapLib={maplibregl}
       initialViewState={{
-        longitude: -74.00,
+        longitude: -74.0,
         latitude: 40.74,
         zoom: 10,
       }}
@@ -40,16 +50,19 @@ const MapPage = () => (
       <Source
         id="bus-routes"
         type="geojson"
-        data={"http://localhost:8000/api/v1/bus-routes"}
+        data={`${DOMAIN}/api/v1/bus-routes`}
       >
         <Layer {...busRouteLayerStyle} />
       </Source>
-      <Source
-        id="bus-stops"
-        type="geojson"
-        data={`${DOMAIN}/api/v1/bus-stops`}
-      >
+      <Source id="bus-stops" type="geojson" data={`${DOMAIN}/api/v1/bus-stops`}>
         <Layer {...busStopLayerStyle} />
+      </Source>
+      <Source
+        id="bus-routes-express"
+        type="geojson"
+        data={`${DOMAIN}/api/v1/bus-routes-express`}
+      >
+        <Layer {...busRouteExpressLayerStyle} />
       </Source>
     </ReactMapGL>
   </Box>
