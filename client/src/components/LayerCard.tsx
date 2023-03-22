3acf0f6e5724a0a76eb0a21bf8eedfd151ef9e49
tangoyankee <miller.tim108@gmail.com>
2023-03-21 21:52:16 -0400

    add layer display controls

---
+++ client/src/components/LayerCard.tsx
@@ -0,0 +1,51 @@
+import { Card, CardBody, Checkbox, Flex } from "@chakra-ui/react";
+import { ChangeEvent } from "react";
+import { useMap } from "react-map-gl";
+
+interface LayerCardProps {
+  addToLoadedSources: () => void;
+  isSourceLoaded: boolean;
+  layerId: string;
+  children: React.ReactNode;
+}
+
+export const LayerCard = ({
+  children,
+  addToLoadedSources,
+  isSourceLoaded,
+  layerId,
+}: LayerCardProps) => {
+  const { sdssMap } = useMap();
+
+  const setLayerVisibility = (e: ChangeEvent<HTMLInputElement>) => {
+    const map = sdssMap.getMap();
+    const isChecked = e.target.checked;
+    if (isChecked) {
+      if (!isSourceLoaded) {
+        addToLoadedSources();
+      } else {
+        map.setLayoutProperty(layerId, "visibility", "visible");
+      }
+    } else {
+      if (!isChecked && isSourceLoaded) {
+        map.setLayoutProperty(layerId, "visibility", "none");
+      }
+    }
+  };
+
+  return (
+    <Card>
+      <CardBody>
+        <Flex direction="column">
+          {children}
+          <Checkbox
+            onChange={setLayerVisibility}
+            defaultChecked={isSourceLoaded}
+          >
+            Show
+          </Checkbox>
+        </Flex>
+      </CardBody>
+    </Card>
+  );
+};
