f9eca7e4f732e2e742882b483c6d62e15dfa92b5
Tim Miller <miller.tim108@gmail.com>
2023-04-09 12:13:46 -0400

    add backend support for track demographics in rankings

--- api/ada_stations/views.py
+++ api/ada_stations/views.py
@@ -95,22 +95,38 @@ class TractDemographicViewSet(viewsets.ModelViewSet):
     serializer_class = TractDemographicSerializer
 
 
+# constants that should not be redefined on every rankings request
+station_buffer = 500
+batch_size = 80
+ada_min_code = 3
+counted_factors_map = {
+    "parks": Park,
+    "schools": School,
+    "hospitals": Hospital,
+    "bus_stops": BusStop,
+    "bus_stops_express": BusStopExpress,
+}
+
+tract_factors = [
+    "total",
+    "under_five",
+    "sixty_five_and_over",
+    "poverty_total",
+    "poverty_under_five",
+    "poverty_sixty_five_and_over",
+    "under_eighteen_ambulatory",
+    "over_eighteen_under_sixty_five_ambulatory",
+    "sixty_five_and_over_ambulatory",
+]
+
+
 class RankingView(APIView):
     def get(self, request):
-        station_buffer = 500
-        batch_size = 80
-        ada_min_code = 3
-        counted_factors_map = {
-            "parks": Park,
-            "schools": School,
-            "hospitals": Hospital,
-            "bus_stops": BusStop,
-            "bus_stops_express": BusStopExpress,
-        }
-
         query_string = request.GET
-        all_factors = list(counted_factors_map.keys())
-        factor_weights = { factor:int(query_string.get(factor) or 0) for factor in all_factors }
+        all_factors = list(counted_factors_map.keys()) + tract_factors
+        factor_weights = {
+            factor: int(query_string.get(factor) or 0) for factor in all_factors
+        }
 
         total_weight = sum(factor_weights.values())
         # Use 1 as fallback for total factor weights, to prevent division by 0
@@ -123,37 +139,59 @@ class RankingView(APIView):
             ada_status_code__gte=ada_min_code
         )
 
-        counts = {}
+        station_totals = {}
         meta_data = {}
+        requested_tract_factors = [
+            factor for factor in tract_factors if factor_weights[factor] > 0
+        ]
+        requested_counted_factors = {
+            factor: model
+            for factor, model in counted_factors_map.items()
+            if factor_weights[factor] > 0
+        }
+        requested_factors = (
+            list(requested_counted_factors.keys()) + requested_tract_factors
+        )
         for index, station in enumerate(stations):
-            count = {}
-            # Find totals for countable factors
-            for factor, model in counted_factors_map.items():
-                if factor_weights[factor] > 0:
-                    factor_count = model.objects.filter(
-                        geom__distance_lte=(station.geom, station_buffer)
-                    ).count()
-                    count[factor] = factor_count
+            # Default totals for requested values
+            factor_totals = {factor: 0 for factor in requested_factors}
 
-                counts[station.id] = count
+            # Find totals for countable factors
+            for factor, model in requested_counted_factors.items():
+                factor_count = model.objects.filter(
+                    geom__distance_lte=(station.geom, station_buffer)
+                ).count()
+                factor_totals[factor] = factor_count
+
+            # Find sums for tract factors
+            tracts = TractDemographic.objects.all().filter(
+                geom__distance_lte=(station.geom, station_buffer)
+            )
+            for tract in tracts:
+                for tract_factor in requested_tract_factors:
+                    factor_totals[tract_factor] += getattr(tract, tract_factor)
+
             meta_data[station.id] = {
                 "id": station.id,
                 "name": station.name,
                 "lines": station.lines,
                 "ada_status_code": station.ada_status_code,
             }
+            station_totals[station.id] = factor_totals
 
-        max_counts = {}
-        for count in counts.values():
+        max_factor_totals = {}
+        for count in station_totals.values():
             for key, value in count.items():
                 # Use 1 for fallback of max, as a hack to prevent division by zero errors
-                max_counts[key] = max([value, max_counts.get(key, 1)])
+                max_factor_totals[key] = max([value, max_factor_totals.get(key, 1)])
 
         scores = {}
-        for station_id, factor_counts in counts.items():
+        for station_id, station_factor_totals in station_totals.items():
             score = 0
-            for factor, count in factor_counts.items():
-                score += (count / max_counts[factor]) * proportional_weights[factor]
+            for factor, count in station_factor_totals.items():
+                score += (count / max_factor_totals[factor]) * proportional_weights[
+                    factor
+                ]
             scores[station_id] = score
 
         ranked_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
@@ -167,7 +205,7 @@ class RankingView(APIView):
                 "score": score,
                 "ranking": ranking,
                 "batch": batch,
-                **counts[id],
+                **station_totals[id],
             }
 
         return Response(rankings)
