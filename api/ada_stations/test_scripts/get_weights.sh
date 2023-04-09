b9069e5113b26005887a7f76e80248433251fd99
Tim Miller <miller.tim108@gmail.com>
2023-04-09 07:49:51 -0400

    add option to test for 0 weight factors and single factor queries

--- api/ada_stations/test_scripts/get_weights.sh
+++ api/ada_stations/test_scripts/get_weights.sh
@@ -6,7 +6,9 @@ PARKS=60
 SCHOOLS=40
 HOSPITALS=80
 BUS_STOPS=50
-BUS_STOPS_EXPRESS=30
+# BUS_STOPS_EXPRESS=30
+# Factor has weight of zero
+BUS_STOPS_EXPRESS=0
 
 # Full query string
 QUERY_STRING="parks=${PARKS}\
@@ -20,6 +22,9 @@ bus_stops_express=${BUS_STOPS_EXPRESS}"
 # &hospitals=${HOSPITALS}&\
 # bus_stops=${BUS_STOPS}&\
 # bus_stops_express=${BUS_STOPS_EXPRESS}"
+
+#Query string with only one factor
+# QUERY_STRING="parks=${PARKS}"
 
 # Query string with all parameters missing
 # QUERY_STRING=""
