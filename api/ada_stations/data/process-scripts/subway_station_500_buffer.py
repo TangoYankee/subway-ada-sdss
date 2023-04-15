da7123e91da03ff1045fcbbe39f95598e09b23be
Tim Miller <miller.tim108@gmail.com>
2023-04-14 21:47:54 -0400

    integrate ridership into station buffer data

--- api/ada_stations/data/process-scripts/subway_station_500_buffer.py
+++ api/ada_stations/data/process-scripts/subway_station_500_buffer.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 
+# Amenities and demographics
 BUFFER = 0.005
 count_factors_files = {
     "schools": "schools_20192020.geojson",
@@ -37,4 +38,28 @@ for index, station_geometry in enumerate(stations.geometry):
 for factor, totals in stations_totals.items():
     stations[factor] = totals
 
-print(stations.head())
+# Station ridership
+station_ridership = gpd.read_file("../subway_stations_2019.geojson")
+
+## Distance at which available ridership data is matched with a station
+MAX_DISTANCE = 6.4e-4
+nearest_station_query = station_ridership.sindex.nearest(
+    geometry=stations.geometry,
+    return_all=False,
+    return_distance=True,
+)
+
+
+nearest_station_indexes = nearest_station_query[0][1]
+nearest_station_distance = nearest_station_query[1]
+riderships = [None] * len(nearest_station_indexes)
+for index, nearest_station_index in enumerate(nearest_station_indexes):
+    riderships[index] = (
+        int(station_ridership.loc[nearest_station_index, "tot2019"])
+        if (nearest_station_distance[index] <= MAX_DISTANCE)
+        else None
+    )
+
+stations["ridership_2019"] = riderships
+
+stations.to_file("subway_stations_500_buffer.geojson", driver="GeoJSON")
