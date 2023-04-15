0daf7d6c0044d63044547fca92da1c0d7182f9df
Tim Miller <miller.tim108@gmail.com>
2023-04-14 20:28:29 -0400

    calculate tract demo totals

--- api/ada_stations/data/process-scripts/subway_station_500_buffer.py
+++ api/ada_stations/data/process-scripts/subway_station_500_buffer.py
@@ -15,41 +15,26 @@ count_factors_data = {factor: None for factor in count_factors_files.keys()}
 for factor_name, factor_file in count_factors_files.items():
     count_factors_data[factor_name] = gpd.read_file(f"../{factor_file}").to_crs(4326)
 
-station_factor_totals = {factor: [] for factor in count_factors_files.keys()}
-## TODO: add tracts_demo factors with empty arrays to station factors
 tracts_demos = tracts.drop(columns=["GEOID", "geometry"])
+tract_factors = [factor for factor in tracts_demos]
+factors = list(count_factors_files.keys()) + tract_factors
+stations_totals = {factor: [] for factor in factors}
 for index, station_geometry in enumerate(stations.geometry):
     station_buffer = station_geometry.buffer(BUFFER)
     for factor, data in count_factors_data.items():
-        station_factor_totals[factor].append(
+        stations_totals[factor].append(
             len(data.sindex.query(station_buffer, predicate="intersects"))
         )
-    tracts_demo_in_buffer = tracts_demos.sindex.query(
-        station_buffer, predicate="intersection"
-    )
+    tracts_in_buffer = tracts.sindex.query(station_buffer, predicate="intersects")
+    station_tract_totals = {factor: 0 for factor in tract_factors}
+    for tract_index in tracts_in_buffer:
+        tract = tracts_demos.iloc[tract_index]
+        for factor in tract_factors:
+            station_tract_totals[factor] += int(tract[factor])
+    for factor, total in station_tract_totals.items():
+        stations_totals[factor].append(total)
 
-    for factor in tracts_demo_in_buffer:
-        ## TODO: accumulate demographic totals among all tracts in station buffer.
-        ## Then append count to station_factors
-        # factor_totals = station_factor_totals.get(factor, [None]*len(stations.geometry))
-        # factor_totals[index] = factor_totals[index] +
-        pass
-
-        pass
-for factor, totals in station_factor_totals.items():
+for factor, totals in stations_totals.items():
     stations[factor] = totals
 
-# print(stations.head())
-tracts_demos = tracts.drop(columns=["GEOID", "geometry"])
-# print(tracts[0])
-for column in tracts_demos:
-    for data in tracts[column]:
-        print(data)
-#     for column in tracts_demos:
-#         print(tract[column].sum())
-# tracts_in_buffer = tracts.sindex.query(station_buffer, predicate="intersects")
-# schools_in_buffer = len(schools.sindex.query(station_buffer, predicate="intersects"))
-# print(schools_in_buffer)
-# # tracts_within_buffer = tracts.query(geometry.intersects(station_buffer), engine="python")
-# # print(tracts.geometry[0].distance( stations.geometry[0] ))
-# # tract.distance()
+print(stations.head())
