522a05e29080fe8548042225ec4ed9352fdaa240
tangoyankee <miller.tim108@gmail.com>
2023-04-29 15:23:31 -0400

    calculate betweeness centrality for stations

--- api/ada_stations/data/process-scripts/subway_station_centrality.py
+++ api/ada_stations/data/process-scripts/subway_station_centrality.py
@@ -1,12 +1,14 @@
 import geopandas as gpd
+import pandas as pd
+import networkx as nx
 
 stations_read = gpd.read_file("./subway_station_accessibility.geojson")
-stations = stations_read.reindex(["complex_id", "line", "geometry"], axis=1)[0:50]
+stations = stations_read.reindex(["complex_id", "line", "geometry"], axis=1)
 
-station_lines = stations['line']
+station_lines = stations["line"]
 unique_station_lines = set()
 for line in station_lines:
-    lines = line.split('-')
+    lines = line.split("-")
     for line in lines:
         unique_station_lines.add(line)
 
@@ -24,21 +26,41 @@ for unique_line, _stations in stations_on_lines.items():
         station_df = _stations[_stations.complex_id == station_complex_id]
 
         nearest_neighbor = gpd.sjoin_nearest(station_df, neighbor_stations)
-        try:
-            # print(f'station: {station_complex_id}')
-            nearest_neighbor_index = nearest_neighbor.iloc[0, 3]
-            nearest_neighbor_id = nearest_neighbor.iloc[0, 4]
-            # print(f'nearest: { nearest_neighbor_id }')
+        nearest_neighbor_index = nearest_neighbor.iloc[0, 3]
+        nearest_neighbor_id = nearest_neighbor.iloc[0, 4]
 
-            further_neighbor_stations = neighbor_stations.drop([nearest_neighbor_index])
-            second_nearest_neighbor = gpd.sjoin_nearest(station_df, further_neighbor_stations)
-            second_nearest_neighbor_id = second_nearest_neighbor.iloc[0, 4]
-            # print(f'second nearest: { second_nearest_neighbor_id }')
-        
-        except:
-            print("index out of range")
+        further_neighbor_stations = neighbor_stations.drop([nearest_neighbor_index])
+        second_nearest_neighbor = gpd.sjoin_nearest(
+            station_df, further_neighbor_stations
+        )
+        second_nearest_neighbor_id = second_nearest_neighbor.iloc[0, 4]
 
-        line_connections[station_complex_id] = [nearest_neighbor_id, second_nearest_neighbor_id]
+        line_connections[station_complex_id] = [
+            nearest_neighbor_id,
+            second_nearest_neighbor_id,
+        ]
+
     network_connections[unique_line] = line_connections
 
-print(network_connections)
+unique_edges = set()
+for _stations in network_connections.values():
+    for station, neighbors in _stations.items():
+        for neighbor in neighbors:
+            edge = tuple(sorted([station, neighbor]))
+            if edge not in unique_edges:
+                neighbors_neighbors = _stations[neighbor]
+                if station in neighbors_neighbors:
+                    unique_edges.add(edge)
+
+edges = list(unique_edges)
+
+subway_graph = nx.Graph()
+subway_graph.add_edges_from(edges)
+subway_centrality = nx.betweenness_centrality(subway_graph)
+
+subway_centrality_df = pd.DataFrame({
+    "complex_id": subway_centrality.keys(),
+    "centrality": subway_centrality.values(),
+})
+
+subway_centrality_df.to_csv("subway_station_centrality.csv")
