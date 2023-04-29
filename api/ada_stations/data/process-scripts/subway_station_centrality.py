e2bbf899819d01b08b42f1c8d701fa94c34c20ef
tangoyankee <miller.tim108@gmail.com>
2023-04-29 12:53:31 -0400

    find first and second nearest subway stations

--- api/ada_stations/data/process-scripts/subway_station_centrality.py
+++ api/ada_stations/data/process-scripts/subway_station_centrality.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
 
 stations_read = gpd.read_file("./subway_station_accessibility.geojson")
-stations = stations_read.reindex(["complex_id", "line", "geometry"], axis=1)
+stations = stations_read.reindex(["complex_id", "line", "geometry"], axis=1)[0:25]
 
 station_lines = stations['line']
 unique_station_lines = set()
@@ -15,4 +15,27 @@ for unique_line in unique_station_lines:
     stations_on_line = stations[stations.line.str.contains(unique_line)]
     stations_on_lines[unique_line] = stations_on_line
 
-print(stations_on_lines)
+station_connections = {}
+for unique_line, _stations in stations_on_lines.items():
+    for index, _complex_id in enumerate(_stations['complex_id']):
+        station = _stations[_stations.complex_id == _complex_id]
+        neighbor_stations = _stations[_stations.complex_id != _complex_id]
+
+        nearest_neighbor_joined = gpd.sjoin_nearest(station, neighbor_stations)
+        nearest_right = nearest_neighbor_joined['complex_id_right']
+        if(len(nearest_right) == 1):
+            for nearest_neighbor_id in nearest_right:
+                print(f'nearest: {nearest_neighbor_id}')
+                further_neighbor_stations = neighbor_stations[neighbor_stations.complex_id != nearest_neighbor_id]
+                second_nearest_neighbor_joined = gpd.sjoin_nearest(station, further_neighbor_stations)
+                second_nearest_right = second_nearest_neighbor_joined['complex_id_right']
+                if(len(second_nearest_right) == 1):
+                    for second_nearest_neighbor_id in second_nearest_right:
+                        print(f'second nearest: {second_nearest_neighbor_id}')
+            
+        # print(nearest_neighbor_joined['complex_id_right'])
+        # print(f'second nearest: {second_nearest_neighbor_id}')
+
+        # print(len(station['complex_id']))
+        # print(len(neighbor_stations['complex_id']))
+        # print(len(_stations['complex_id']))
