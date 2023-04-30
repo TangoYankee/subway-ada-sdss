609883148b9345563e6507d38a5f5f6ec59df153
tangoyankee <miller.tim108@gmail.com>
2023-04-30 08:28:27 -0400

    update test script to include station traits

--- api/ada_stations/test_scripts/get_weights.sh
+++ api/ada_stations/test_scripts/get_weights.sh
@@ -22,6 +22,11 @@ UNDER_EIGHTEEN_AMBULATORY=40
 OVER_EIGHTEEN_UNDER_SIXTY_FIVE_AMBULATORY=30
 SIXTY_FIVE_AND_OVER_AMBULATORY=90
 
+#Trait factor weights
+RIDERSHIP=30
+ADA_NEIGHBOR_GAP=50
+BETWEENNESS_CENTRALITY=70
+
 # Count factors query string
 # QUERY_STRING="parks=${PARKS}\
 # &schools=${SCHOOLS}\
@@ -47,6 +52,22 @@ SIXTY_FIVE_AND_OVER_AMBULATORY=90
 # &sixty_five_and_over_ambulatory=${SIXTY_FIVE_AND_OVER_AMBULATORY}"
 
 # Tract and count factors
+# QUERY_STRING="total=${TOTAL}\
+# &under_five=${UNDER_FIVE}\
+# &sixty_five_and_over=${SIXTY_FIVE_AND_OVER}\
+# &poverty_total=${POVERTY_TOTAL}\
+# &poverty_under_five=${POVERTY_UNDER_FIVE}\
+# &poverty_sixty_five_and_over=${POVERTY_SIXTY_FIVE_AND_OVER}\
+# &under_eighteen_ambulatory=${UNDER_EIGHTEEN_AMBULATORY}\
+# &over_eighteen_under_sixty_five_ambulatory=${OVER_EIGHTEEN_UNDER_SIXTY_FIVE_AMBULATORY}\
+# &sixty_five_and_over_ambulatory=${SIXTY_FIVE_AND_OVER_AMBULATORY}\
+# &parks=${PARKS}\
+# &schools=${SCHOOLS}\
+# &hospitals=${HOSPITALS}\
+# &bus_stops=${BUS_STOPS}\
+# &bus_stops_express=${BUS_STOPS_EXPRESS}"
+
+# Tract, count, and trait factors
 QUERY_STRING="total=${TOTAL}\
 &under_five=${UNDER_FIVE}\
 &sixty_five_and_over=${SIXTY_FIVE_AND_OVER}\
@@ -60,7 +81,11 @@ QUERY_STRING="total=${TOTAL}\
 &schools=${SCHOOLS}\
 &hospitals=${HOSPITALS}\
 &bus_stops=${BUS_STOPS}\
-&bus_stops_express=${BUS_STOPS_EXPRESS}"
+&bus_stops_express=${BUS_STOPS_EXPRESS}\
+&ridership=${RIDERSHIP}\
+&ada_neighbor_gap=${ADA_NEIGHBOR_GAP}\
+&betweenness_centrality=${BETWEENNESS_CENTRALITY}"
+
 
 # Query string with only one factor
 # QUERY_STRING="parks=${PARKS}"
