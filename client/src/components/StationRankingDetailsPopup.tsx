d83f28b04431bed3b9c61d940c426fb822ea247e
tangoyankee <miller.tim108@gmail.com>
2023-05-07 11:41:32 -0400

    reduce information on station popup

--- client/src/components/StationRankingDetailsPopup.tsx
+++ client/src/components/StationRankingDetailsPopup.tsx
@@ -1,19 +1,9 @@
-import {
-  Box,
-  Heading,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
+import { Box, Heading, Text, Flex } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
 import { Popup, useMap } from "react-map-gl";
 import { RankingsContext } from "../context/RankingsContext";
 import { LAYER_ID } from "../helpers/MapLayers";
-import { Ranking, SubwayStationAdaProperties } from "../types";
+import { SubwayStationAdaProperties } from "../types";
 
 export const ADA_STATUS = [
   "Full",
@@ -31,9 +21,8 @@ export const StationRankingDetailsPopup = () => {
   const [showPopup, setShowpopup] = useState(false);
   const [longitude, setLongitude] = useState(CENTER_LNG);
   const [latitude, setLatitude] = useState(CENTER_LAT);
-  const { rankings, subwayStationAdaMap, complexId, setComplexId } =
+  const { subwayStationAdaMap, complexId, setComplexId } =
     useContext(RankingsContext);
-  const [ranking, setRanking] = useState<Ranking | null>(null);
   const [station, setStation] = useState<SubwayStationAdaProperties | null>(
     null
   );
@@ -61,15 +50,12 @@ export const StationRankingDetailsPopup = () => {
   }, [sdssMap, setComplexId, setShowpopup]);
 
   useEffect(() => {
-    let _ranking = null;
     let _station = null;
     if (complexId) {
-      if (rankings) _ranking = rankings[complexId];
       if (subwayStationAdaMap) _station = subwayStationAdaMap[complexId];
     }
 
     setShowpopup(complexId ? true : false);
-    setRanking(_ranking);
     setStation(_station);
 
     const _latitude = _station ? _station.lat : CENTER_LAT;
@@ -77,7 +63,7 @@ export const StationRankingDetailsPopup = () => {
 
     const _longitude = _station ? _station.lng : CENTER_LNG;
     setLongitude(_longitude);
-  }, [subwayStationAdaMap, rankings, complexId]);
+  }, [subwayStationAdaMap, complexId]);
 
   const onPopupClose = () => {
     setShowpopup(false);
@@ -92,41 +78,10 @@ export const StationRankingDetailsPopup = () => {
       maxWidth="fit-content"
     >
       {station ? (
-        <TableContainer>
-          <Heading size="xs">
-            {`${station.name} Station on ${station.lines} lines`}
-          </Heading>
-          <Table size="sm" variant="striped">
-            <Thead>
-              <Tr>
-                <Th>Factor</Th>
-                <Th>Value</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>ADA Status</Td>
-                <Td>{ADA_STATUS[station.ada_status_code]}</Td>
-              </Tr>
-              <Tr>
-                <Td>Score</Td>
-                <Td>
-                  {ranking
-                    ? `${(ranking.score * 100).toFixed(0)}%`
-                    : RANKING_UNAVAILABLE}
-                </Td>
-              </Tr>
-              <Tr>
-                <Td>Ranking</Td>
-                <Td>{ranking ? ranking.ranking : RANKING_UNAVAILABLE}</Td>
-              </Tr>
-              <Tr>
-                <Td>Phase</Td>
-                <Td>{ranking ? ranking.batch + 1 : RANKING_UNAVAILABLE}</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
+        <Flex direction="column">
+          <Heading size="xs">{`${station.name} Station`}</Heading>
+          <Text>{`${station.lines} lines`}</Text>
+        </Flex>
       ) : (
         <Box>Station data unavailable</Box>
       )}
